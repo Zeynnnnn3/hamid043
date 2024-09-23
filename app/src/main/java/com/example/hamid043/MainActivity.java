8b72164bea239b72c5ce683802220efe58e775f9
Zeynnnnn3 <zeyntagizade@gmail.com>
2024-09-23 00:46:31 -0700

    i added change colour button

--- app/src/main/java/com/example/hamid043/MainActivity.java
+++ app/src/main/java/com/example/hamid043/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.hamid043;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvViewTheText;
+    Button button;
+    Button button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+       this.tvViewTheText = findViewById(R.id.viewthetext);
+       this.button = findViewById(R.id.button);
+       this.button.setOnClickListener(view -> tvViewTheText.setText("after clicking"));
+       this.button4.setOnClickListener(view -> button4.setTextColor(Color.parseColor("#FF0000")));
     }
 }
